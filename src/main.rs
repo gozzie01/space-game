a1582b1ac24d78bd700a3e244d4f455126e65617
MapleC-Dev <c4012131@ncl.ac.uk>
2024-09-27 18:53:55 +0100

    Add place body on click

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::vec;
 use bevy::input::mouse::MouseButtonInput;
 use bevy::sprite::{Wireframe2dConfig, Wireframe2dPlugin};
+use bevy::transform::commands;
 use bevy::{
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
@@ -235,6 +236,9 @@ fn mouse_system(
     windows: Query<&Window>,
     camera_q: Query<(&Camera, &GlobalTransform), With<Camera>>,
     mouse_button_input: Res<ButtonInput<MouseButton>>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let window = windows.single();
     let (camera, camera_transform) = camera_q.single();
@@ -244,5 +248,20 @@ fn mouse_system(
         .and_then(|cursor| camera.viewport_to_world_2d(camera_transform, cursor))
     {
         println!("Mouse: {:?}", mouse_button_input.get_pressed().collect::<Vec<_>>());
+        let d_world_position = DVec2::new(world_position.x as f64, world_position.y as f64) * 1e9;
+        let radius = 2.0;
+        if mouse_button_input.just_pressed(MouseButton::Left) {
+            commands.spawn((
+                Position((d_world_position)),
+                Velocity(DVec2::zero()),
+                Mass(5.972e24),
+                MaterialMesh2dBundle {
+                    mesh: meshes.add(Circle { radius }).into(),
+                    material: materials.add(ColorMaterial::from(Color::WHITE)),
+                    transform: Transform::from_translation(Vec3::new(d_world_position.x as f32, d_world_position.y as f32, 0.0)),
+                    ..default()
+                },
+            ));
+        }
     }
 }
